72ccb44a500882971e05922ac9ed1e19259de9c5
hongtai zhang <qq251569880@gmail.com>
2014-09-23 21:37:51 +0800

    调试
    调试

--- GameScene.swift
+++ GameScene.swift
@@ -18,6 +18,8 @@ class GameScene: SKScene {
     var blueControl = SKSpriteNode();
     var whiteBall = SKSpriteNode();
 
+    var lastPosition:CGPoint = CGPointMake(0,0);
+    var selectedNode:SKNode?;
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         var skyColor = SKColor();
@@ -39,7 +41,7 @@ class GameScene: SKScene {
         
         //它的位置是左右居中。Scene的Y轴是由下至上的，所以是屏幕高度除以2+100，即中间靠上的位置
         backgroundBoard.position = CGPointMake(self.frame.size.width/2,backgroundBoard.size.height/2+50)
-        backgroundBoard.zPosition = 1;
+        //backgroundBoard.zPosition = 1;
         boardPosition = backgroundBoard.position
         self.addChild(backgroundBoard)
         //添加物理边界
@@ -49,25 +51,28 @@ class GameScene: SKScene {
         var redTexture = SKTexture(imageNamed: "red");
         redControl = SKSpriteNode(texture:redTexture);
         redControl.setScale(screen.bounds.size.width/backgroundBoard.size.width*0.5);
-        redControl.physicsBody = SKPhysicsBody.bodyWithCircleOfRadius(redControl.size.width/2);
-        redControl.physicsBody.dynamic = ture;
-        redControl.position = CGPointMake( boardPosition.x,boardPosition.y - boardSize.height/2)
+        redControl.physicsBody = SKPhysicsBody(circleOfRadius:redControl.size.width/2);
+        redControl.physicsBody!.dynamic = true;
+        redControl.physicsBody!.affectedByGravity = false
+        redControl.position = CGPointMake( boardPosition.x,boardPosition.y - boardSize.height/4)
         self.addChild(redControl)
 
         var blueTexture = SKTexture(imageNamed: "blue");
         blueControl = SKSpriteNode(texture:blueTexture);
         blueControl.setScale(screen.bounds.size.width/backgroundBoard.size.width*0.5);
-        blueControl.physicsBody = SKPhysicsBody.bodyWithCircleOfRadius(blueControl.size.width/2);
-        blueControl.physicsBody.dynamic = ture;
-        blueControl.position = CGPointMake( boardPosition.x,boardPosition.y + boardSize.height/2)
+        blueControl.physicsBody = SKPhysicsBody(circleOfRadius:blueControl.size.width/2);
+        blueControl.physicsBody!.dynamic = true;
+        blueControl.physicsBody!.affectedByGravity = false
+        blueControl.position = CGPointMake( boardPosition.x,boardPosition.y + boardSize.height/4)
         self.addChild(blueControl)
 
         var ballTexture = SKTexture(imageNamed: "ball");
         whiteBall = SKSpriteNode(texture:ballTexture);
         whiteBall.setScale(screen.bounds.size.width/backgroundBoard.size.width*0.5);
-        whiteBall.physicsBody = SKPhysicsBody.bodyWithCircleOfRadius(whiteBall.size.width/2);
+        whiteBall.physicsBody = SKPhysicsBody(circleOfRadius:whiteBall.size.width/2);
         whiteBall.position = CGPointMake( boardPosition.x,boardPosition.y)
-        whiteBall.physicsBody.dynamic = ture;
+        whiteBall.physicsBody!.dynamic = true;
+        whiteBall.physicsBody!.affectedByGravity = false
         self.addChild(whiteBall)
     }
     
@@ -75,22 +80,31 @@ class GameScene: SKScene {
         /* Called when a touch begins */
         
         for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+            lastPosition = touch.locationInNode(self)
+            selectedNode = self.nodeAtPoint(lastPosition);
         }
     }
-   
+    override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+        
+        for touch: AnyObject in touches {
+            var location = touch.locationInNode(self)
+            if(selectedNode != nil){
+                var action = SKAction();
+                action = SKAction.moveTo(location,duration:0.01);
+                if(selectedNode === redControl){
+                    redControl.runAction(action);
+                }
+            }
+            lastPosition = location;
+        }
+    }
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+        
+        selectedNode = nil;
+    }
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
@@ -108,22 +122,22 @@ class GameScene: SKScene {
         var left2 = CGPointMake(leftEdgex,bottomy);
         var left3 = CGPointMake(leftEdgex,topy);
         var left4 = CGPointMake(leftDoorx,topy);
-        var edgePath =CGPathCreateMutable();  
-        CGPathMoveToPoint(edgePath, NULL, left1.x, left1.y);
-        CGPathAddLineToPoint(edgePath,NULL,left2.x,left2.y);
-        CGPathAddLineToPoint(edgePath,NULL,left3.x,left3.y);
-        CGPathAddLineToPoint(edgePath,NULL,left4.x,left4.y);
+        var edgePath = CGPathCreateMutable();
+        CGPathMoveToPoint(edgePath, nil, left1.x, left1.y);
+        CGPathAddLineToPoint(edgePath,nil,left2.x,left2.y);
+        CGPathAddLineToPoint(edgePath,nil,left3.x,left3.y);
+        CGPathAddLineToPoint(edgePath,nil,left4.x,left4.y);
 
         var right1 = CGPointMake( rightDoorx,bottomy );
         var right2 = CGPointMake(rightEdgex,bottomy);
         var right3 = CGPointMake(rightEdgex,topy);
         var right4 = CGPointMake(rightDoorx,topy);
-        var rightPath =CGPathCreateMutable();  
-        CGPathMoveToPoint(edgePath, NULL, right1.x, right1.y);
-        CGPathAddLineToPoint(edgePath,NULL,right2.x,right2.y);
-        CGPathAddLineToPoint(edgePath,NULL,right3.x,right3.y);
-        CGPathAddLineToPoint(edgePath,NULL,right4.x,right4.y);
+        var rightPath = CGPathCreateMutable();
+        CGPathMoveToPoint(edgePath, nil, right1.x, right1.y);
+        CGPathAddLineToPoint(edgePath,nil,right2.x,right2.y);
+        CGPathAddLineToPoint(edgePath,nil,right3.x,right3.y);
+        CGPathAddLineToPoint(edgePath,nil,right4.x,right4.y);
 
-        backgroundBoard.physicsBody = SKPhysicsBody.bodyWithEdgeChainFromPath(edgePath);
+        backgroundBoard.physicsBody = SKPhysicsBody(edgeChainFromPath:edgePath);
     }
 }
